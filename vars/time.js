ba4d5cd5cc1c05d5e3c1a99e136aad2727a8193f
AndreVanKammen <andrevankammen@gmail.com>
2021-09-14 17:34:26 +0200

    time by format function

--- vars/time.js
+++ vars/time.js
@@ -1,23 +1,10 @@
+import { secondsToTimeStr } from "../../KMN-utils.js/format.js";
 import { BaseDefinition } from "./base.js";
 import { FloatVar } from "./float.js";
 
 class TimeVar extends FloatVar {
   get $niceStr() {
-    // https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
-    var date = new Date(0);
-    date.setSeconds(this._value); // specify value for SECONDS here
-    var timeString = date.toISOString().substr(11, 8);
-
-    let ix = 0;
-    while (ix < timeString.length-4) {
-      let c = timeString.charAt(ix);
-      if ((c !== '0') && (c !== ':')) {
-        break;
-      }
-      ix++;
-    }
-    
-    return timeString.substr(ix);
+    return secondsToTimeStr(this._value);
   }
   // TODO: Add extra converter for setValue that handles HH:MM:SS.ZZZ from string
 
