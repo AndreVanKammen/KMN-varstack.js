150f32f9293607f3e2d78e7c193c959a7f1a00d2
Andre van Kammen <andrevankammen@gmail.com>
2022-05-24 20:59:36 +0200

    added table view class for sort and filter

--- structures/table.js
+++ structures/table.js
@@ -13,10 +13,6 @@ class TableVar extends BaseVar {
 
     /** @type {typeof TableVar} */ /* @ts-ignore: Don't blame ts for not getting this :) */
     this.myProto = Reflect.getPrototypeOf(this);
-    this.sortField = '';
-    this.sortAscending = false;
-    this.sortInvalidated = false;
-    this.sortArray = [];
     this.inLinkChanged = true;
   }
 
@@ -206,91 +202,6 @@ class TableVar extends BaseVar {
     return -1;
   }
 
-  /**
-   * 
-   * @param {string} fieldName 
-   * @param {string | number} value1 
-   * @param {number} value2 
-   */
-  setFilter(fieldName, value1, value2) {
-    this.sortField = fieldName;
-    this.filterField = fieldName;
-    this.filterValueStr = value1.toString();
-    // @ts-ignore
-    this.filterValue1 = Math.min(value1, value2);
-    // @ts-ignore
-    this.filterValue2 = Math.max(value1, value2);
-    this.sortInvalidated = true;
-  }
-
-  setSort(fieldName, ascending = undefined) {
-    console.log('set sort:', fieldName);
-    if (this.sortField !== fieldName) {
-      this.sortField = fieldName;
-      this.sortInvalidated = true;
-    } else {
-      this.sortAscending = !this.sortAscending;
-      this.sortInvalidated = true;
-    }
-    if (ascending !== undefined && this.sortAscending !== ascending) {
-      this.sortAscending = ascending;
-      this.sortInvalidated = true;
-    }
-  }
-
-  getSortArray() {
-    if (this.sortInvalidated) {
-      this.sortArray = [];
-      if (this.sortField === '') {
-        for (let ix = 0; ix < this.length; ix++) {
-          this.sortArray.push(ix);
-        }
-      } else {
-        let tempArray = [];
-        for (let ix = 0; ix < this.length; ix++) {
-          let val = this.element(ix)[this.sortField].$sortValue;
-          if (this.filterField) {
-            let fieldIx = this.elementType.prototype._fieldNames.indexOf(this.filterField);
-            let recInFiltered = true;
-            if (this.elementType.prototype._fieldDefs[fieldIx].sortIsNumber) {
-              if (this.element(ix)[this.filterField].$sortValue < this.filterValue1) {
-                recInFiltered = false;
-              }
-              if (this.element(ix)[this.filterField].$sortValue > this.filterValue2) {
-                recInFiltered = false;
-              }
-            } else {
-              recInFiltered = this.element(ix)[this.filterField].$niceStr.toLocaleLowerCase()
-                .indexOf(this.filterValueStr.toLocaleLowerCase()) >= 0;
-            }
-
-            if (recInFiltered) {
-              tempArray.push({ ix, val });
-            }
-          }
-          else {
-            tempArray.push({ ix, val });
-          }
-        }
-
-
-        let fieldIx = this.elementType.prototype._fieldNames.indexOf(this.sortField);
-        if (fieldIx >= 0) {
-          let sortMultiplier = this.sortAscending ? -1 : 1;
-          if (this.elementType.prototype._fieldDefs[fieldIx].sortIsNumber) {
-            tempArray = tempArray.sort((a, b) => (a.val - b.val) * sortMultiplier);
-          } else {
-            tempArray = tempArray.sort((a, b) => a.val.localeCompare(b.val) * sortMultiplier)
-          }
-          for (let ix = 0; ix < tempArray.length; ix++) {
-            this.sortArray.push(tempArray[ix].ix)
-          }
-        }
-      }
-      this.sortInvalidated = false;
-    }
-    return this.sortArray;
-  }
 }
 TableVar.elementDef = undefined;
 
@@ -406,7 +317,6 @@ class ArrayTableVar extends TableVar {
   
   handleArrayChanged () {
     this.tableChanged();
-    this.sortInvalidated = true;
 
     for (let callBack of this._arrayChangedCallbacks) {
       if (callBack) {
