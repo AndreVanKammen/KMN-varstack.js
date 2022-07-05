37c8ac02f089a289aab0ccde1a349923c7061497
AndreVanKammen <andrevankammen@gmail.com>
2022-07-05 16:08:14 +0200

    Duration shorter then a seconds shown in ms

--- vars/duration.js
+++ vars/duration.js
@@ -4,6 +4,12 @@ import { TimeVar } from "./time.js";
 // Duration is a special case for a time var
 // TODO: Add a string function that handles days like 3d 10 min 5 seconds or something like that
 export class DurationVar extends TimeVar {
+  get $niceStr() {
+    if (this._value < 1.0) {
+      return (this._value * 1000.0).toFixed((this._value < 100.0) ? 1 : 0) + 'ms';
+    }
+    return super.$niceStr;
+  }
 }
 
 DurationVar.typeDefinition = new BaseDefinition({
