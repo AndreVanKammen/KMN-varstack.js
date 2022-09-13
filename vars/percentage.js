557cc9ed941053e8f280c51dc3111622b3542b78
AndreVanKammen <andrevankammen@gmail.com>
2022-09-13 11:08:59 +0200

    Percentage 0 and 100 ithout precission

--- vars/percentage.js
+++ vars/percentage.js
@@ -4,6 +4,12 @@ import { FloatVar } from "./float.js";
 
 export class PercentageVar extends FloatVar {
   get $niceStr() {
+    if (this._value >= 1.0) {
+      return '100%'
+    }
+    if (this._value <= 0.0) {
+      return '0%'
+    }
     return (this._value * 100).toFixed(this.$varDefinition.precision) + '%';
   }
   // TODO: Add extra converter for setValue that handles HH:MM:SS.ZZZ from string
